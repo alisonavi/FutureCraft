b366a08f0de7a133d85fd9b0d354391c8a834c46
alisonavi <bsaulebay@gmail.com>
2025-05-19 17:25:35 +0500

    preference page adding photos

--- src/PrefenceTest page/PreferencePage.jsx
+++ src/PrefenceTest page/PreferencePage.jsx
@@ -1,11 +1,13 @@
 // src/pages/PreferencePage.jsx
 import React from 'react';
 import Preference from '../PrefenceTest page/Preference';
-
+import img1 from '../assets/goofy1.jpg'
+import img2 from '../assets/goofy2.jpg'
+import img3 from '../assets/goofy3.jpg'
+import img4 from '../assets/goofy4.jpg'
 const testItems = [
-  { id: '1', left: '/imgs/a1.jpg', right: '/imgs/b1.jpg' },
-  { id: '2', left: '/imgs/a2.jpg', right: '/imgs/b2.jpg' },
-  // …more pairs
+  { id: '1', left: img1, right: img2 },
+  { id: '2', left: img3, right: img4 },
 ];
 
 export default function PreferencePage() {
