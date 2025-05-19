82adeeaca3cd3c893b742417a2ad5914842ee716
alisonavi <bsaulebay@gmail.com>
2025-05-19 17:54:36 +0500

    fixing the bugs

--- src/Reusable components/NavBar.jsx
+++ src/Reusable components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Navbar.css';
+import './NavBar.css';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import logo from '../assets/FutureCraftLogo.png';
 const Navbar = () => {
