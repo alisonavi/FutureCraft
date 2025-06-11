f4a4867a59ff24083acdff83884fee9e9f997b48
alisonavi <bsaulebay@gmail.com>
2025-06-11 10:42:38 +0500

    fixed the bug in eachModule.css for deploy

--- src/eachCourse/eachModule.jsx
+++ src/eachCourse/eachModule.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import './EachModule.css';
+import './eachModule.css';
 
 const EachModule = () => {
   const { occupationId, moduleId } = useParams();
